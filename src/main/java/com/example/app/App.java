3de0bb5e1b06cfe3e66000447775e22f6789073a
Oluwasegun Lasisi <lomikun@gmail.com>
2026-08-18 15:43:09 +0100

    Turn Java app into HTTP service

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,7 +1,40 @@
 package com.example.app;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello from my Java Gradle DevOps project!");
+
+    public static void main(String[] args) throws IOException {
+
+        int port = 8080;
+
+        HttpServer server = HttpServer.create(
+                new InetSocketAddress(port),
+                0
+        );
+
+        server.createContext("/", exchange -> {
+
+            String response = "Hello from Java Gradle DevOps!";
+
+            exchange.sendResponseHeaders(
+                    200,
+                    response.getBytes().length
+            );
+
+            try (OutputStream outputStream = exchange.getResponseBody()) {
+                outputStream.write(response.getBytes());
+            }
+        });
+
+        server.start();
+
+        System.out.println(
+                "Java application running on port " + port
+        );
     }
 }
